69848e0f54c6fb73cd646cb68cfc57a067a71bf3
Manoj <mks148@gmail.com>
2016-02-25 10:27:21 -0500

    Second paragraph Added

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,12 +1,23 @@
 // setup your IIFE (Immediately Invoked Function Expression)
  (function () {
  //if (document.getElementById("IntroParagraph") != null) {
- 
+ //intro paragraph code
     var introparagraph;
 
     introparagraph = document.getElementById("IntroParagraph");
 
     introparagraph.innerHTML = "Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals. Greece has 1,400 islands, though only 230 of them are inhabited And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too.";
+})();
+ 
+ //Folegandros paragraph code
+(function () {
+
+
+    var folegandro;
+
+    folegandro = document.getElementById("Folegandros");
+
+    folegandro.innerHTML = "Folegandros is almost a Greek cliché, full of beautiful whitewashed houses with bright blue doors lining cobblestoned streets on steep cliffs. Rugged and remote, without chain hotels or package tours — not even a bank or ATM — and accessible only by ferry, the volcanic island boasts solitude in spades, secluded beaches, and incredible sunsets..";
 })();
 
 
